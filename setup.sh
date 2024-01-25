2fa4906c7d942844b8e9fc7dbeb106adf8d6aea1
Rezky Eko Bawono <71922987+rezkyekobawono@users.noreply.github.com>
2024-01-25 17:55:40 +0700

    Setup

---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+echo   "L7 Setup"
+read -sp "Press [ENTER] To Start."
+apt update -y
+apt upgrade -y
+apt install screen -y
+apt install htop -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install pip -y
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
+apt install npm -y
+npm install request
+npm install sync-request
+npm install cloudscraper
+npm install random-string
+npm install https-proxy-agent
+npm install axios
+npm install fake-useragent
+npm install random-ua
+npm install proxy-agent
+npm install request
+npm install puppeteer-extra
+npm i puppeteer
+npm install puppeteer-extra-plugin-stealth
+npm i requests
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+pip3 install -r requirements.txt
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb -y
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -;sudo apt -y install nodejs
+sudo apt -y install gcc g++ make
+sudo apt -y install htop vnstat
+sudo apt-get install -y unzip
+sudo apt -y install unrar
+npm i await-timeout
+npm i console-log-level
+npm i events
+npm i hpagent
+npm i got
+npm i crypto
+npm i cheerio
+npm i url
+npm i axios
+npm i https
+npm i request-curl
+npm i user-agents
+npm i uuid
+npm i tmp-promise
+npm i puppeteer-extra
+npm i puppeteer-extra-plugin-stealth
+npm i sleep-promise
+npm i puppeteer-extra-plugin-adblocker
+npm install puppeteer
+sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo apt-get install -y libgbm-dev
+chmod 777 *
+ulimit -n 999999
+chmod 777 *
+reboot
